8e36ad85d232704288a79e43034716f43b595040
ansel <anselhall@163.com>
2015-10-15 23:45:22 +0800

    Initial commit

---
+++ app/src/main/java/com/qyz/qyztest/MyGradientText.java
@@ -0,0 +1,66 @@
+package com.qyz.qyztest;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Ansel on 2015/10/15.
+ */
+public class MyGradientText extends TextView {
+
+    private int mViewWidth;
+    private Paint paint;
+    private LinearGradient mLinearGradient;
+    private Matrix mGradientMatrix;
+    private int mTranslate;
+
+    public MyGradientText(Context context) {
+        super(context);
+    }
+
+    public MyGradientText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyGradientText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                paint = getPaint();
+                mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0, new int[]{Color.BLUE, 0xffffff, Color.BLUE}, null, Shader.TileMode.CLAMP);
+
+                paint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mGradientMatrix != null) {
+            mTranslate += mViewWidth / 5;
+
+            if (mTranslate > 2 * mViewWidth) {
+                mTranslate = -mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate,0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(100);
+        }
+    }
+}
