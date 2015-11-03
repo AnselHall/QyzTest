01ddf0940ea109d46b1b8801b1666bbb2d4d5cab
ansel <anselhall@163.com>
2015-11-03 23:11:54 +0800

    111

--- app/src/main/java/com/qyz/qyztest/MainActivity.java
+++ app/src/main/java/com/qyz/qyztest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.qyz.qyztest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,8 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-
+    public void compoundView(View view) {
+        startActivity(new Intent(this,CompoundViewTest.class));
     }
 
 }
